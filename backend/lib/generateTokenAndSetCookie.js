451e337225f916e80eb6248cd22fb05e05ba3f76
Tareq Radi <raditareq15@gmail.com>
2025-06-29 18:35:58 +0300

    fix

--- backend/lib/generateTokenAndSetCookie.js
+++ backend/lib/generateTokenAndSetCookie.js
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '1d',
     });
+    const isProduction = process.env.NODE_ENV === 'production' || 
+                    process.env.VERCEL_ENV === 'production';
+
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        secure: isProduction,
+        sameSite: isProduction ? 'none' : 'lax',
+        maxAge: 24 * 60 * 60 * 1000,
         path: '/',
-        domain: process.env.NODE_ENV === 'production' ? '.tourvisto-nu.vercel.app' : undefined
+        domain: isProduction ? '.tourvisto-nu.vercel.app' : undefined
     });
     return token;
 }
