440ac9a95d11694a728b6541384c889fbff48bb1
Tareq Radi <raditareq15@gmail.com>
2025-06-29 16:53:10 +0300

    edit type for params

--- frontend/src/app/trip/[tripId]/page.tsx
+++ frontend/src/app/trip/[tripId]/page.tsx
@@ -1,26 +1,26 @@
 import { getUserInSession } from '@/app/_action';
 import { redirect } from 'next/navigation';
-import React from 'react';
 import Navbar from './components/Navbar';
 import TripContent from './components/TripContent';
 
-const TripPage = async ({ params }: { params: { tripId: string } }) => {
+const TripPage = async ({ params }: { params: Promise<{tripId: string}> }) => {
     const { user, token } = await getUserInSession();
     if (!token) return redirect('/login');
 
-    const { tripId } = params;
-    
+    const { tripId } = await params;
+
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/trips/${tripId}`, {
         headers: {
-            Cookie: `token=${token}`,
-        }
+        Cookie: `token=${token}`,
+        },
     });
     const data = await res.json();
+
     return (
-        <div>
-            <Navbar user={user} />
-            <TripContent initialData={data} user={user} />
-        </div>
+        <>
+        <Navbar user={user} />
+        <TripContent initialData={data} user={user} />
+        </>
     );
 };
 
