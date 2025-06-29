02bce09e1b9ffb27c736bb7a9f07462b1580a55d
Tareq Radi <raditareq15@gmail.com>
2025-06-29 18:03:35 +0300

    fix sameSite

--- backend/lib/generateTokenAndSetCookie.js
+++ backend/lib/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        sameSite: 'none', // Prevent CSRF attacks
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
     return token;
