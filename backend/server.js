009440e48be881f78c6268481dbba644a530b573
Tareq Radi <raditareq15@gmail.com>
2025-06-29 17:29:57 +0300

    fix my cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: [process.env.CLIENT_URL],
+    origin: ['https://tourvisto-nu.vercel.app'],
     credentials: true,
 }));
 
